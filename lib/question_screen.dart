be8d28822db59566860ac0f52b68a30428ba7e90
Ömer Okumuş <omerokumus3@gmail.com>
2023-05-28 17:24:46 +0300

    QuestionScreen is created

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -11,6 +11,18 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text("Question Screen");
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Question', style: TextStyle(color: Colors.white)),
+          const SizedBox(height: 20),
+          ElevatedButton(onPressed: () {}, child: const Text("Answer 1")),
+          ElevatedButton(onPressed: () {}, child: const Text("Answer 2")),
+          ElevatedButton(onPressed: () {}, child: const Text("Answer 3")),
+        ],
+      ),
+    );
   }
 }
